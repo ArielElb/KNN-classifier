a39fc10965b74504a452f8f5e0e743a17e288f37
Erel Arnold <erelarnold@Erels-MacBook-Pro.local>
2023-01-11 10:09:44 +0200

    Adding command class files

---
+++ Command.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erel Arnold on 1/11/23.
+//
+
+#include "Command.h"
---
+++ Command.h
@@ -0,0 +1,10 @@
+#ifndef ASS4_COMMAND_H
+#define ASS4_COMMAND_H
+
+
+class Command {
+
+};
+
+
+#endif //ASS4_COMMAND_H
