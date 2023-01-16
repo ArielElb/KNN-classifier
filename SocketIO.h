e81060afe5e6a1003cab8b2e692efd53308e3725
Erel Arnold <erelarnold@Erels-MacBook-Pro.local>
2023-01-16 17:19:04 +0200

    Working SocketIO - Read documentation in SocketIO.h file

--- SocketIO.h
+++ SocketIO.h
@@ -1,24 +1,41 @@
-//
-// Created by arie1 on 1/14/2023.
-//
-
 #ifndef ASS4_SOCKETIO_H
 #define ASS4_SOCKETIO_H
 
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
 #include <string>
 #include "DefaultIO.h"
 
 class SocketIO : public DefaultIO {
+    int sockfd;
     // bindSocket , listen , accept , read , write , close - methods
+public:
+    /*
+     * HOW TO USE - read():
+     * Always wrap call to read() in try catch statement as such"
+     * SocketIO sio(socketfd);
+     * try {
+     *     sio.read();    
+     * } catch (std::ios_base::failure const &ex) {
+     *     // This exception indicates failure reading from socket
+     *     Handle error here
+     * } catch (std::out_of_range const &ex) {
+     *     // This exception indicates communication was disconnected by other party
+     *     Handle behavior here
+     * }
+     */
     std::string read() override;
-    void write(std::string string) override;
+    int write(std::string string) override;
 
-    virtual  int bindSocket(int port) = 0;
     explicit SocketIO(int sockfd) {
         this->sockfd = sockfd;
     }
-    virtual ~SocketIO() = default;
-    int sockfd;
+    ~SocketIO() = default;
 };
 
 
