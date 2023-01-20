877e7faafc225cf65b45a0a6db06046c52987248
Erel Arnold <erelarnold@Erels-MacBook-Pro.local>
2023-01-20 16:10:58 +0200

    Linking error fixed, Makefile added

--- Distances/AUC.cpp
+++
@@ -1,9 +0,0 @@
-#include "AUC.h"
-
-void AUC::operator()(Vector &a, Vector &b) {
-    Vector absDiff;
-    absDiff.init(a.absValueDiff(b));
-    a.setDistFromArg(Vector::euclideanDistance(absDiff));
-}
-AUC::AUC() = default;
-AUC::~AUC() = default;
--- Distances/CHB.cpp
+++
@@ -1,9 +0,0 @@
-#include "CHB.h"
-
-void CHB::operator()(Vector &a, Vector &target) {
-    Vector absDiff;
-    absDiff.init(a.absValueDiff(target));
-    a.setDistFromArg(Vector::chebyshevDistance(absDiff));
-}
-CHB::CHB() = default;
-CHB::~CHB() = default;
